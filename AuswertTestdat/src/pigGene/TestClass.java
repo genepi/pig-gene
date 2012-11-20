1c0f52b6b586791db6fff5e6f9cd685ae3b30e8f
clembanas <clemens.banas@me.com>
2012-11-20 15:00:00 +0100

    extract geneinfo from reference file - TODO: discuss handling of different information columns

--- AuswertTestdat/src/pigGene/TestClass.java
+++ AuswertTestdat/src/pigGene/TestClass.java
@@ -11,13 +11,15 @@ package pigGene;
 public class TestClass {
 
 	public static void main(String[] args) {
-		String a = "file1.txt";
-		String b = "file10.txt";
-		if(a.compareTo(b) < 0) {
-			System.out.println("smaller");
-		} else {
-			System.out.println("larger");
-		}
+		// String a = "file1.txt";
+		// String b = "file10.txt";
+		// if(a.compareTo(b) < 0) {
+		// System.out.println("smaller");
+		// } else {
+		// System.out.println("larger");
+		// }
+		String blub = "drxtGENEINFO=blub";
+		System.out.println(blub.indexOf("GENEINFO="));
 
 	}
 
