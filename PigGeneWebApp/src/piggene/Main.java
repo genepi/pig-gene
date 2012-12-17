e8477a4199aedfbe2977339be77ad98010b153f7
clemens <clemens@think-cl>
2012-12-17 11:03:49 +0100

    mod

--- PigGeneWebApp/src/piggene/Main.java
+++ PigGeneWebApp/src/piggene/Main.java
@@ -6,24 +6,16 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class Main {
-
 	private static final Log log = LogFactory.getLog(Main.class);
 
 	public static void main(String[] args) throws IOException {
-
 		try {
-
 			int port = 8080;
-
 			new WebServer(port).start();
-
 		} catch (Exception e) {
-
-			log.error("Can't launch the web server.\nAn unexpected "
-					+ "exception occured:", e);
-
+			log.error("Can't launch the web server.\nAn unexpected exception occured:", e);
 			System.exit(1);
-
 		}
 	}
+	
 }
