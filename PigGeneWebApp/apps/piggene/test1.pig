c6e87761b9d8d3346565a64d7810ed483e38b81c
clembanas <clemens.banas@me.com>
2013-05-07 16:32:45 +0200

    yaml file creation for cloudgene added

--- PigGeneWebApp/apps/piggene/test1.pig
+++ PigGeneWebApp/apps/piggene/test1.pig
@@ -1,2 +1,6 @@
 REGISTER pigGene.jar;
-R1 = LOAD '$input' USING pigGene.storage.merged.PigGeneStorage();
+R1 = LOAD '$in1' USING pigGene.storage.merged.PigGeneStorage();
+R2 = LOAD '$in2' USING pigGene.storage.merged.PigGeneStorage();
+R3 = FILTER asdf BY asdf;
+STORE sdf INTO '$out1';
+STORE wet INTO '$out2';
--- PigGeneWebApp/src/piggene/serialisation/yaml/CloudgeneYaml.java
+++ PigGeneWebApp/src/piggene/serialisation/yaml/CloudgeneYaml.java
@@ -3,8 +3,10 @@ package piggene.serialisation.yaml;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 import piggene.serialisation.Workflow;
+import piggene.serialisation.WorkflowComponent;
 
 import com.esotericsoftware.yamlbeans.YamlWriter;
 
@@ -13,14 +15,67 @@ public class CloudgeneYaml {
 
 	public static void generateCloudgeneYamlFile(final Workflow workflow) throws IOException {
 		final App app = new App();
+		StringBuilder parameters = new StringBuilder();
+		final ArrayList<Parameter> inputs = new ArrayList<Parameter>();
+		final ArrayList<Parameter> outputs = new ArrayList<Parameter>();
+
+		final ArrayList<WorkflowComponent> workflowDefinition = workflow.getWorkflow();
+		for (final WorkflowComponent wfc : workflowDefinition) {
+			if (wfc.getOperation().equals("LOAD")) {
+				parameters = appendParameter(parameters, wfc.getInput());
+				inputs.add(createInputParameter(wfc.getInput()));
+			} else if (wfc.getOperation().equals("STORE")) {
+				parameters = appendParameter(parameters, wfc.getRelation());
+				outputs.add(createOutputParameter(wfc.getRelation()));
+			}
+		}
+
+		final Step step = new Step();
+		step.setName("PigScript");
+		step.setPig(workflow.getName());
+		step.setParams(parameters.toString().trim());
+
+		final ArrayList<Step> steps = new ArrayList<Step>();
+		steps.add(step);
+
+		final MapReduceConfig mapred = new MapReduceConfig();
+		mapred.setSteps(steps);
+		mapred.setInputs(inputs);
+		mapred.setOutputs(outputs);
+
 		app.setName(workflow.getName());
 		app.setDescription(workflow.getDescription());
 		app.setVersion("1.0.0");
 		app.setCategory("Piggene");
+		app.setMapred(mapred);
 
 		final YamlWriter writer = new YamlWriter(new OutputStreamWriter(new FileOutputStream(PATH.concat(workflow.getName().concat(".yaml")))));
 		writer.write(app);
 		writer.close();
+	}
+
+	private static StringBuilder appendParameter(final StringBuilder parameters, final String parameterName) {
+		return parameters.append("-param ").append(parameterName).append("=$").append(parameterName).append(" ");
+	}
+
+	private static Parameter createInputParameter(final String parameterName) {
+		final InputParameter param = new InputParameter();
+		param.setId(parameterName);
+		param.setDescription(parameterName);
+		param.setType("hdfs-folder");
+		return param;
+	}
+
+	private static Parameter createOutputParameter(final String parameterName) {
+		final OutputParameter param = new OutputParameter();
+		param.setId(parameterName);
+		param.setDescription(parameterName);
+		param.setType("hdfs-folder");
+		param.setDownload(true);
+		param.setTemp(false);
+		param.setZip(false);
+		param.setMergeOutput(false);
+		return param;
 	}
 
 }
