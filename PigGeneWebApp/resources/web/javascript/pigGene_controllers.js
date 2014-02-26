7156bd3e3a31e9602a1be640a2a4dde0e8c9af6f
clembanas <clemens.banas@me.com>
2014-02-26 17:01:02 +0100

    optical tweaks

--- PigGeneWebApp/resources/web/javascript/pigGene_controllers.js
+++ PigGeneWebApp/resources/web/javascript/pigGene_controllers.js
@@ -3,35 +3,14 @@ pigGeneApp.run(function(editableOptions) {
 	  editableOptions.theme = 'bs3'; // bootstrap3 theme
 });
 
-
 pigGeneApp.controller("NavBarCtrl", function($scope) {
-	$scope.buttons = [
-	   {name:"newWfBtn", title:"create new workflow", showState:true, text:"new"},
-	   {name:"showWfBtn", title:"open existing workflow", showState:true, text:"open"},
-	   {name:"saveWfBtn", title:"save workflow", showState:true, text:"save"},
-	   {name:"deleteWfBtn", title:"delete workflow", showState:true, text:"delete"},
-	   {name:"downloadScriptBtn", title:"download pig script", showState:true, text:"download"},
-	   {name:"runJobBtn", title:"run workflow on cluster", showState:true, text:"run"}
-	];
+	$scope.buttons = buttons;
 });
-
-
 
 pigGeneApp.controller('EditableRowCtrl', function($scope, $filter, $http) {
 	$scope.workflow = workflow;
-
-	$scope.operations = [
-		{name: 'REGISTER'},
-		{name: 'LOAD'},
-		{name: 'FILTER'},
-		{name: 'JOIN'},
-		{name: 'SELECT'},
-		{name: 'GROUP BY'},
-		{name: 'ORDER BY'},
-		{name: 'USER SCRIPT'},
-		{name: 'STORE'}
-	]; 
-
+	$scope.operations = operations;
+	
 	$scope.showStatus = function(step,index) {
 		if(step.operation && step.operation.name) {
 			workflow[index].operation = step.operation.name;
@@ -54,6 +33,5 @@ pigGeneApp.controller('EditableRowCtrl', function($scope, $filter, $http) {
 			active: false
 		};
 		$scope.workflow.push($scope.inserted);
-	};
-	
+	};	
 });
--- PigGeneWebApp/resources/web/javascript/pigGene_model.js
+++ PigGeneWebApp/resources/web/javascript/pigGene_model.js
@@ -1,5 +1,26 @@
+var buttons =  [
+		{name:"newWfBtn", title:"create new workflow", showState:true, active:"active", text:"new", logo:"file"},
+		{name:"showWfBtn", title:"open existing workflow", showState:true, active:"", text:"open", logo:"folder-open"},
+		{name:"saveWfBtn", title:"save workflow", showState:true, active:"", text:"save", logo:"floppy-disk"},
+		{name:"deleteWfBtn", title:"delete workflow", showState:true, active:"", text:"delete", logo:"trash"},
+		{name:"downloadScriptBtn", title:"download pig script", showState:true, active:"", text:"download", logo:"cloud-download"},
+		{name:"runJobBtn", title:"run workflow on cluster", showState:true, active:"", text:"run", logo:"play"}
+	];
+
+var operations = [
+		{name: 'REGISTER'},
+		{name: 'LOAD'},
+		{name: 'FILTER'},
+		{name: 'JOIN'},
+		{name: 'SELECT'},
+		{name: 'GROUP BY'},
+		{name: 'ORDER BY'},
+		{name: 'USER SCRIPT'},
+		{name: 'STORE'}       
+	];
+
 var workflow = [
-    {relation:"R1", input:"input1", input2:"-", operation:"LOAD", options:"vcf", options2:"-", comment:"Loads the input file.", active:false},
-    {relation:"R2", input:"R1", input2:"-", operation:"FILTER", options:"pos==138004", options2:"-", comment:"Filters all lines that match position '138004'.", active:false},
-    {relation:"R3", input:"R2", input2:"", operation:"STORE", options:"-", options2:"-", comment:"-", active:false}          
- ];
+	    {relation:"R1", input:"input1", input2:"-", operation:"LOAD", options:"vcf", options2:"-", comment:"Loads the input file.", active:false},
+	    {relation:"R2", input:"R1", input2:"-", operation:"FILTER", options:"pos==138004", options2:"-", comment:"Filters all lines that match position '138004'.", active:false},
+	    {relation:"R3", input:"R2", input2:"-", operation:"STORE", options:"-", options2:"-", comment:"-", active:false}          
+    ];
