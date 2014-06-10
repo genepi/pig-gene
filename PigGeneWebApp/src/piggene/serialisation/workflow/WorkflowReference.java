4d0261ca673700b35867a39b768ec9dfb20ba17d
Clemens Banas <clemens.banas@me.com>
2014-06-10 17:23:19 +0200

    added indentation for subworkflows in the pig script

--- PigGeneWebApp/src/piggene/serialisation/workflow/WorkflowReference.java
+++ PigGeneWebApp/src/piggene/serialisation/workflow/WorkflowReference.java
@@ -6,6 +6,7 @@ import piggene.serialisation.pig.DynamicInputParameterMapper;
 import piggene.serialisation.pig.DynamicOutputParameterMapper;
 
 public class WorkflowReference extends Workflow {
+	private static int indentation = 0;
 	private static WorkflowType workflowType = WorkflowType.WORKFLOW_REFERENCE;
 
 	private String name;
@@ -39,6 +40,7 @@ public class WorkflowReference extends Workflow {
 
 	@Override
 	public String getPigScriptRepresentation(final boolean renameParam, final String wfName) throws IOException {
+		WorkflowReference.indentation++;
 		String workflowName = this.name;
 		Workflow referencedWorkflow = WorkflowSerialisation.load(workflowName);
 		DynamicInputParameterMapper.addParamMapping(referencedWorkflow.getInputParameterMapping(), workflowName);
@@ -46,14 +48,26 @@ public class WorkflowReference extends Workflow {
 
 		StringBuilder sb = new StringBuilder();
 		sb.append(System.getProperty("line.separator"));
+		sb.append(insertIndentationTabs());
 		sb.append(parseInfo(workflowName));
+		sb.append(insertIndentationTabs());
 		sb.append(parseInfo(referencedWorkflow.getDescription()));
 
 		for (Workflow wf : referencedWorkflow.getSteps()) {
 			sb.append(System.getProperty("line.separator"));
+			sb.append(insertIndentationTabs());
 			sb.append(wf.getPigScriptRepresentation(true, workflowName));
 		}
 		sb.append(System.getProperty("line.separator"));
+		WorkflowReference.indentation = 0;
+		return sb.toString();
+	}
+
+	private String insertIndentationTabs() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < WorkflowReference.indentation; i++) {
+			sb.append("\t");
+		}
 		return sb.toString();
 	}
 
