54ec06881d8c89f06f0486b37442de95dfa6cfb7
Clemens Banas <clemens.banas@me.com>
2014-06-11 08:56:06 +0200

    bugfixes - pig script generation - store operation

--- PigGeneWebApp/src/piggene/serialisation/workflow/LoadOperation.java
+++ PigGeneWebApp/src/piggene/serialisation/workflow/LoadOperation.java
@@ -1,7 +1,6 @@
 package piggene.serialisation.workflow;
 
 import piggene.serialisation.pig.DynamicInputParameterMapper;
-import piggene.serialisation.pig.DynamicOutputParameterMapper;
 
 public class LoadOperation extends Workflow implements IWorkflowOperation {
 	private static WorkflowType workflowType = WorkflowType.WORKFLOW_SINGLE_ELEM;
@@ -88,9 +87,10 @@ public class LoadOperation extends Workflow implements IWorkflowOperation {
 	@Override
 	public String getPigScriptRepresentation(final boolean renameParam, final String wfName) {
 		String mappedInputValue = DynamicInputParameterMapper.getMappedValue(wfName, input);
-		if (mappedInputValue == null) {
-			mappedInputValue = DynamicOutputParameterMapper.getMappedValue(wfName, input);
-		}
+		// if (mappedInputValue == null) {
+		// mappedInputValue =
+		// DynamicOutputParameterMapper.getMappedValue(wfName, input);
+		// }
 
 		StringBuilder sb = new StringBuilder();
 		sb.append(parseInfo(getComment()));
