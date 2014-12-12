e72a6a5605bc8b8b6895e13e2892109cc5028bc2
Clemens Banas <clemens.banas@me.com>
2014-12-12 11:01:15 +0100

    extended model by "expert-mode" button

--- PigGeneWebApp/resources/web/javascript/model.js
+++ PigGeneWebApp/resources/web/javascript/model.js
@@ -2,7 +2,8 @@ var buttons =  [
 		{name:"newWfBtn", title:"create new workflow", showState:true, active:"", text:"new", logo:"file"},
 		{name:"openWfBtn", title:"open existing workflow", showState:true, active:"", text:"open", logo:"folder-open"},
 		{name:"deleteWfBtn", title:"delete workflow", showState:true, active:"", text:"delete", logo:"trash"},
-		{name:"downloadScriptBtn", title:"download pig script", showState:true, active:"", text:"download", logo:"cloud-download"}
+		{name:"downloadScriptBtn", title:"download pig script", showState:true, active:"", text:"download", logo:"cloud-download"},
+		{name:"expertModeBtn", title:"toggle display mode", showState:true, active:"", text:"expert-mode", logo:"tasks"}
 	];
 
 var scriptType = [
